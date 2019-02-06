7e34f956db4c3f487e379955fc2562652e88433c
LeTerrienDu27 <42359806+LeTerrienDu27@users.noreply.github.com>
2019-02-07 00:02:09 +0100

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -37,4 +37,29 @@ bot.on("message", async function(message) {
     }
 });
 
-bot.login("NTQyNzk0OTc2NTU2MzUxNDkx.Dzzq9A.YcQREx14bfllKfNYzzlyUyASw50");
+let kUser = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
+if(!kUser) return message.channel.send("Can't find user!");
+let kReason = args.join(" ").slice(22);
+if(!message.member.hasPermission("MANAGE_MESSAGES")) return message.channel.send("No can do pal!");
+if(kUser.hasPermission("MANAGE_MESSAGES")) return message.channel.send("That person can't be kicked!");
+
+let kickEmbed = new Discord.RichEmbed()
+.setDescription("~Kick~")
+.setColor("#e56b00")
+.addField("Kicked User", `${kUser} with ID ${kUser.id}`)
+.addField("Kicked By", `<@${message.author.id}> with ID ${message.author.id}`)
+.addField("Kicked In", message.channel)
+.addField("Tiime", message.createdAt)
+.addField("Reason", kReason);
+
+let kickChannel = message.guild.channels.find(`name`, "incidents");
+if(!kickChannel) return message.channel.send("Can't find incidents channel.");
+
+message.guild.member(kUser).kick(kReason);
+kickChannel.send(kickEmbed);
+
+return;
+}
+
+
+bot.login("NTQyNzk0OTc2NTU2MzUxNDkx.Dzz4ww.-YBDyBATuHm5RvfXpW5a10oplr0");
